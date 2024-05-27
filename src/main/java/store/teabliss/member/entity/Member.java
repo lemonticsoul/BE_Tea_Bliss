6923e0641e27daec1f279efba46fcdcac2fbf225
Mun Kyoung-min <mun9659@naver.com>
2024-05-27 20:55:22 +0900

    [feat] 사용자 관련 파일 Init, Security 설정 Init

---
+++ src/main/java/store/teabliss/member/entity/Member.java
@@ -0,0 +1,18 @@
+package store.teabliss.member.entity;
+
+
+public class Member {
+
+    private int id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String nickname;
+
+    private String profile;
+
+}
---
+++ src/main/java/store/teabliss/member/mapper/MemberMapper.java
@@ -0,0 +1,13 @@
+package store.teabliss.member.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import store.teabliss.member.entity.Member;
+
+import java.util.List;
+
+// @Mapper
+public interface MemberMapper {
+
+    // List<Member> findByMembers();
+
+}
