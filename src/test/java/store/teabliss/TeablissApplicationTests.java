b98424b3ac5a12c6e8060b2c0a5c2afdb422af30
Mun Kyoung-min <mun9659@naver.com>
2024-05-27 20:16:34 +0900

    Tea-Bliss Backend Init

---
+++ src/test/java/store/teabliss/TeablissApplicationTests.java
@@ -0,0 +1,13 @@
+package store.teabliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeablissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
