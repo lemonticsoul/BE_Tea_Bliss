122da63f5894c80ae8c7f174b9cb4ec065303193
Mun Kyoung-min <mun9659@naver.com>
2024-06-06 16:52:40 +0900

    [fix] 중복 에러 메시지 수정, 회원가입 Valid 수정

--- src/main/java/store/teabliss/member/exception/MemberStatus.java
+++ src/main/java/store/teabliss/member/exception/MemberStatus.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public enum MemberStatus {
     MEMBER_JOIN_FAIL(400, "회원가입에 실패했습니다."),
-    DUPLICATE_MEMBER(400, "이미 존재하는 회원 입니다."),
-    DUPLICATE_NICKNAME(400, "이미 존재하는 닉네임 입니다."),
+    DUPLICATE_MEMBER(400, "이메일 중복된 회원이 있습니다."),
+    DUPLICATE_NICKNAME(400, "닉네임 중복된 회원이 있습니다."),
     NOT_FOUND_MEMBER_BY_ID(400, "회원 아이디와 일치하는 정보가 없습니다."),
     NOT_FOUND_MEMBER_BY_EMAIL(400, "회원 이메일과 일치하는 정보가 없습니다.");
 
