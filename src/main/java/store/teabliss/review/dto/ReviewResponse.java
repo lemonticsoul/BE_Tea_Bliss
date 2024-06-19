f25ecb92d1110c4134ce79a0ae68fb700248f5e5
Mun Kyoung-min <mun9659@naver.com>
2024-06-19 09:41:47 +0900

    [feat] 리뷰 등록/수정 API 추가 및 Response 변경으로 인한 코드 수정

--- src/main/java/store/teabliss/review/dto/ReviewResponse.java
+++ src/main/java/store/teabliss/review/dto/ReviewResponse.java
@@ -1,29 +1,40 @@
 package store.teabliss.review.dto;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.Builder;
 import lombok.Getter;
 import store.teabliss.common.response.CommonResponse;
-import store.teabliss.ingredient.dto.IngredientResponse;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Getter
+@JsonInclude(Include.NON_NULL)
 public class ReviewResponse extends CommonResponse {
 
     private final Object data;
-    private final boolean isLastPage;
+
+    private final Boolean isLastPage;
+
+    private final Integer total;
 
     @Builder
-    public ReviewResponse(int status, String message, Object data, boolean isLastPage) {
+    public ReviewResponse(int status, String message, Object data, Boolean isLastPage, Integer total) {
         super(status, message);
         this.data = data;
         this.isLastPage = isLastPage;
+        this.total = total;
     }
 
-    public static ReviewResponse ok(String message, Object data, boolean isLastPage) {
-        return new ReviewResponse(200, message, data, isLastPage);
+    public static ReviewResponse ok(String message, Object data, Boolean isLastPage, Integer total) {
+        return new ReviewResponse(200, message, data, isLastPage, total);
     }
 
-    public static ReviewResponse ok(Object data, boolean isLastPage) {
-        return new ReviewResponse(200, "Success", data, isLastPage);
+    public static ReviewResponse ok(Object data, Boolean isLastPage, Integer total) {
+        return new ReviewResponse(200, "Success", data, isLastPage, total);
+    }
+
+    public static ReviewResponse ok(Object data) {
+        return new ReviewResponse(200, "Success", data, null, null);
     }
 
 }
