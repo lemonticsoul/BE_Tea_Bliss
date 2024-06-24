e7c92e581719c810b7a6b48a34b362d3f0de77ab
Mun Kyoung-min <mun9659@naver.com>
2024-06-24 23:14:46 +0900

    [fix] 설문조사 API 추가

--- src/main/java/store/teabliss/survey/service/SurveyService.java
+++ src/main/java/store/teabliss/survey/service/SurveyService.java
@@ -9,6 +9,7 @@ import store.teabliss.survey.mapper.SurveyMapper;
 import store.teabliss.tea.entity.Tea;
 import store.teabliss.tea.mapper.TeaMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,6 +24,29 @@ public class SurveyService {
         Survey survey = surveyCreateDto.toEntity(memId);
 
         return surveyMapper.createSurvey(survey);
+    }
+
+    public List<SurveyDto> findBySurveys() {
+        List<SurveyDto> surveyDtos = new ArrayList<>();
+
+        List<Survey> surveys = surveyMapper.findBySurveys();
+
+        for(Survey s : surveys) {
+            Tea tea = Tea.builder()
+                    .priceStart(s.getSale())
+                    .priceEnd(s.getSale() + 10000)
+                    .category(s.getCategory())
+                    .caffeine(s.getCaffeine().equals("N"))
+                    .build();
+
+            List<Tea> teas = teaMapper.surveyRecommendTea(tea);
+
+            SurveyDto surveyDto = SurveyDto.of(s, teas);
+
+            surveyDtos.add(surveyDto);
+        }
+
+        return surveyDtos;
     }
 
     public SurveyDto findById(Long id) {
